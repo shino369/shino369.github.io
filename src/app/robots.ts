5baa3761f0a27775b315c7af4852e2ae525938a2
SHINO369 <nakagame02@gmail.com>
2023-12-03 19:54:04 +0900

    update sitemap

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,12 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/private/',
+      userAgent: "*",
+      allow: "/",
+      disallow: "/private/",
     },
-    // sitemap: '[domain]/sitemap.xml',
-  }
+    sitemap: process.env.NEXT_PUBLIC_BASEURL + "/sitemap.xml",
+  };
 }
