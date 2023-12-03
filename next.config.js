e6e83179a9b4c58010051e8db52c842f604667a6
SHINO369 <nakagame02@gmail.com>
2023-12-03 16:37:51 +0900

    fix: remove catch all route for static

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const withNextIntl = require("next-intl/plugin")();
 const nextConfig = {};
-
+let envImageUnoptimize = process.env.NODE_ENV !== "production" ? false : true
 module.exports = withNextIntl({
   output: process.env.NODE_ENV !== "production" ? undefined : "export",
   images: {
